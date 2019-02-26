3d823113bc73de150c7d9ec260031a52c32436eb
Pye <pyephyom@gmail.com>
2019-02-25 23:44:03 -0800

    Update dired config

--- init.el
+++ init.el
@@ -122,7 +122,6 @@ Return nil if COMMAND is not found anywhere in `exec-path'."
 (setq epa-pinentry-mode 'loopback)
 
 ;;;; dired
-(setq dired-listing-switches "-alh")
 (defun dired-get-size ()
   (interactive)
   (let ((files (dired-get-marked-files)))
@@ -132,6 +131,48 @@ Return nil if COMMAND is not found anywhere in `exec-path'."
         (progn
           (re-search-backward "\\(^[0-9.,]+[A-Za-z]+\\).*total$")
           (match-string 1))))))
+
+(defun xah-open-in-external-app (&optional @fname)
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference.
+
+When called in emacs lisp, if @fname is given, open that.
+
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2019-01-18"
+  (interactive)
+  (let* (
+         ($file-list
+          (if @fname
+              (progn (list @fname))
+            (if (string-equal major-mode "dired-mode")
+                (dired-get-marked-files)
+              (list (buffer-file-name)))))
+         ($do-it-p (if (<= (length $file-list) 5)
+                       t
+                     (y-or-n-p "Open more than 5 files? "))))
+    (when $do-it-p
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc
+         (lambda ($fpath)
+           (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" $fpath t t))) $file-list))
+       ((string-equal system-type "darwin")
+        (mapc
+         (lambda ($fpath)
+           (shell-command
+            (concat "open " (shell-quote-argument $fpath))))  $file-list))
+       ((string-equal system-type "gnu/linux")
+        (mapc
+         (lambda ($fpath) (let ((process-connection-type nil))
+                            (start-process "" nil "xdg-open" $fpath))) $file-list))))))
+
+(add-hook 'dired-mode-hook
+  (lambda ()
+    (dired-hide-details-mode)
+    (define-key dired-mode-map [return] 'dired-single-buffer)
+    (define-key dired-mode-map [backspace] 'dired-single-up-directory)
+    (define-key dired-mode-map [o] 'xah-open-in-external-app)))
 
 
 (use-package ibuffer)

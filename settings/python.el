83afc7444df4e182b67c40dccbbeb355bcff7d39
Pye <pye@picnichealth.com>
2019-06-17 14:14:10 -0700

    Update python

--- settings/python.el
+++ settings/python.el
@@ -6,7 +6,13 @@
   :init
   (advice-add 'python-mode :before 'elpy-enable)
   :config
+  (setq python-shell-interpreter "python3")
+  (setq python-indent-guess-indent-offset nil)
+  (setq elpy-modules '(elpy-module-sane-defaults
+                        elpy-module-company
+                        elpy-module-eldoc
+                        elpy-module-highlight-indentation
+                        elpy-module-yasnippet))
   (setq flycheck-python-pycompile-executable "python3")
   (setq flycheck-python-pylint-executable "python3")
-  (setq flycheck-python-flake8-executable "python3")
-  (add-hook 'python-mode-hook 'highlight-indent-guides-mode))
+  (setq flycheck-python-flake8-executable "python3"))

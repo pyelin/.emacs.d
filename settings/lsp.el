09292d319fbdca1fca418fe9204628715151e48d
Pye <pye@picnichealth.com>
2020-03-26 15:55:47 -0700

    Configure lsp for python

--- settings/lsp.el
+++ settings/lsp.el
@@ -1,10 +1,14 @@
 (setenv "TSSERVER_LOG_FILE" "/tmp/tsserver.log")
 (use-package lsp-mode
-  :init (setq lsp-keymap-prefix "C-c C-l")
+  :init
+  (setq lsp-keymap-prefix "C-c C-l")
+  (setq lsp-prefer-flymake nil)
+  (setq lsp-pyls-plugins-pycodestyle-enabled nil)
   :hook
   (
     (js2-mode . lsp)
     (sql-mode . lsp)
+    (python-mode . lsp)
   )
   :commands lsp)
 

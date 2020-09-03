258e806e79a8b7e44fc1eaa1eaa4e532a04f666e
Pye <pye@picnichealth.com>
2020-09-03 11:18:47 -0700

    Fix python mode

--- settings/python.el
+++ settings/python.el
@@ -7,14 +7,14 @@
     '(python-pycompile python-pylint python-flake8)))
 
 (use-package anaconda-mode
-  :mode ("\\.py\\'" . json-mode)
+  :mode ("\\.py\\'" . python-mode)
   :config
   (add-hook 'python-mode-hook 'anaconda-mode)
   (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
 
 
 (use-package company-anaconda
-  :mode ("\\.py\\'" . json-mode)
+  :mode ("\\.py\\'" . python-mode)
   :config
   (eval-after-load "company"
     '(add-to-list 'company-backends 'company-anaconda)))

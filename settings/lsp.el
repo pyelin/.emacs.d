f7c398e6cf54b6bdc4b50325350771a548796ffe
Pye <pye@picnichealth.com>
2021-07-12 12:11:32 -0700

    Use tide for typescript since lsp is slow

--- settings/lsp.el
+++ settings/lsp.el
@@ -7,9 +7,7 @@
   (setq lsp-headerline-breadcrumb-enable nil)
 
   :hook
-  ((js2-mode . lsp-deferred)
-    (web-mode . lsp-deferred)
-    (python-mode . lsp-deferred)
+  ((python-mode . lsp-deferred)
     (haskell-mode . lsp-deferred)
     (sql-mode . lsp-deferred))
   :commands lsp)
@@ -18,3 +16,20 @@
 (use-package lsp-treemacs)
 (use-package lsp-haskell)
 
+
+;; typescript using tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1))
+
+(add-hook 'web-mode-hook
+  (lambda ()
+    (when (string-match-p "\\(ts\\|tsx\\)$" (file-name-extension buffer-file-name))
+      (setup-tide-mode))))
+
+(flycheck-add-mode 'typescript-tslint 'web-mode)

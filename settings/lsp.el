42c2ffc02a2a39d79e580b0a84d52c43b843029c
Pye <pye@picnichealth.com>
2021-08-08 16:26:17 -0700

    Replace lsp-mode with eglot

--- settings/lsp.el
+++ settings/lsp.el
@@ -1,35 +1,45 @@
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-l")
-  (setq lsp-prefer-flymake nil)
-  (setq lsp-enable-symbol-highlighting t)
-  (setq lsp-enable-on-type-formatting nil)
-  (setq lsp-headerline-breadcrumb-enable nil)
-
-  :hook
-  ((python-mode . lsp-deferred)
-    (haskell-mode . lsp-deferred)
-    (sql-mode . lsp-deferred))
-  :commands lsp)
-
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs)
-(use-package lsp-haskell)
+(use-package eglot
+  :ensure t
+  :config
+  (add-hook 'web-mode-hook 'eglot-ensure)
+  (add-to-list 'eglot-server-programs '(web-mode . ("typescript-language-server" "--stdio")))
+  (add-to-list 'eglot-server-programs
+             `(python-mode . ("~/.pyenv/shims/pyls" "-v" "--tcp" "--host"
+                              "localhost" "--port" :autoport))))
 
 
-;; typescript using tide
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+;; (use-package lsp-mode
+;;   :init
+;;   (setq lsp-keymap-prefix "C-l")
+;;   (setq lsp-prefer-flymake nil)
+;;   (setq lsp-enable-symbol-highlighting t)
+;;   (setq lsp-enable-on-type-formatting nil)
+;;   (setq lsp-headerline-breadcrumb-enable nil)
 
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1))
+;;   :hook
+;;   ((python-mode . lsp-deferred)
+;;     (haskell-mode . lsp-deferred)
+;;     (sql-mode . lsp-deferred))
+;;   :commands lsp)
 
-(add-hook 'web-mode-hook
-  (lambda ()
-    (when (string-match-p "\\(ts\\|tsx\\)$" (file-name-extension buffer-file-name))
-      (setup-tide-mode))))
+;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+;; (use-package lsp-treemacs)
+;; (use-package lsp-haskell)
 
-(flycheck-add-mode 'typescript-tslint 'web-mode)
+
+;; ;; typescript using tide
+;; (defun setup-tide-mode ()
+;;   (interactive)
+;;   (tide-setup)
+;;   (flycheck-mode +1)
+;;   (setq flycheck-check-syntax-automatically '(save mode-enabled))
+
+;;   (eldoc-mode +1)
+;;   (tide-hl-identifier-mode +1))
+
+;; (add-hook 'web-mode-hook
+;;   (lambda ()
+;;     (when (string-match-p "\\(ts\\|tsx\\)$" (file-name-extension buffer-file-name))
+;;       (setup-tide-mode))))
+
+;; (flycheck-add-mode 'typescript-tslint 'web-mode)

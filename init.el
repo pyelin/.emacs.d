546d39b2af3e9a4559f4eeaa7ee917a74abb9117
Pye <pye@picnichealth.com>
2023-05-05 14:30:36 -0700

    Add tsi

--- init.el
+++ init.el
@@ -299,6 +299,15 @@
 (use-package tree-sitter-langs
   :after tree-treesitter)
 
+(use-package tsi
+  :after tree-sitter
+  :straight '(tsi :type git :host github :repo "orzechowskid/tsi.el")
+  ;; define autoload definitions which when actually invoked will cause package to be loaded
+  :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
+  :init
+  (add-hook 'typescript-mode-hook (λ () (tsi-typescript-mode 1)))
+  (add-hook 'json-mode-hook (λ () (tsi-json-mode 1)))
+  (add-hook 'css-mode-hook (λ () (tsi-css-mode 1))))
 
 (when (memq window-system '(mac x))
   (use-package vterm

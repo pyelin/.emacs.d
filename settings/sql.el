bb30c3ae5334c20868e80e37b702c6165b626080
Pye <pye@numeralhq.com>
2026-03-06 16:30:20 -0800

    Use sqlfluff for formatting

--- settings/sql.el
+++ settings/sql.el
@@ -1,13 +1,7 @@
 (use-package sqlformat
   :commands (sqlformat sqlformat-buffer sqlformat-region)
   :init
-  (setq sqlformat-command 'pgformatter
-    sqlformat-args
-    '("-s2"
-       "-w90"
-       "--nogrouping"
-       "--comma-break"
-       "--format-type"
-       "--keep-newline")))
+  (setq sqlformat-command 'sqlfluff
+    sqlformat-args '("--dialect" "duckdb")))
 
 (use-package csv-mode)
